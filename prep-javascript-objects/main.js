9166fc656f098aae6195e7d69726a26c2d2107f2
Chazz Cordell <chazzcordell123@gmail.com>
2021-11-15 17:17:05 -0800

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,9 @@
+var person = { firstName: 'Chazz', lastName: 'Cordell', hobby: 'Gym' };
+console.log(person);
+var fullname = person.firstName + ' ' + person.lastName;
+console.log(fullname);
+person.job = 'roofing';
+console.log(person.job);
+person.previousJob = 'Server';
+console.log(person.job);
+console.log(person);
